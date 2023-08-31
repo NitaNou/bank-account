6a0a0e1645023e8755cd04b48ba522514c977e2c
b.nou <nou.benita@gmail.com>
2023-08-31 18:51:30 -0400

    updated w/file to read & main to display output

--- src/main/java/org/example/BankAccountApp.java
+++ src/main/java/org/example/BankAccountApp.java
@@ -1,15 +1,32 @@
 package org.example;
 
+import utilities.CSV;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args) {
+
+        /*
         Checking checkingAccount1 = new Checking("Charlie Bucket", "123456789", 2000);
         Savings savingsAccount1 = new Savings("Charlie Bucket", "987654321", 1000);
-
         checkingAccount1.showInfo();
         checkingAccount1.compound();
         System.out.println("*************************");
         savingsAccount1.showInfo();
         savingsAccount1.compound();
+         */
+
+        String path = "/Users/benita.nou/Projects/bank-account/src/main/resources/NewBankAccounts.txt";
+        List<String[]> dataSet = CSV.read(path);
+        for (String[] accountHolder : dataSet) {
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
+        }
+
 
     }
 }
